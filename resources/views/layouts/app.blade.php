d012e0f3df7e2b82f4f4c824db6034c9a5f06650
windys <windysabtami85@gmail.com>
2024-01-18 13:16:29 +0800

    upp

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,9 +41,10 @@
 
     <!-- Bootstrap JS -->
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-beta1/css/bootstrap.min.css" rel="stylesheet">
 
 
-   
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
