01f53bb61e306c126c78efa7fff2fc1108f7ee5e
windys <windysabtami85@gmail.com>
2024-01-18 16:31:14 +0800

    ojke

--- resources/views/gugatan_cerai.blade.php
+++ resources/views/gugatan_cerai.blade.php
@@ -1,30 +1,55 @@
-<!-- resources/views/gugatan_cerai.blade.php -->
-
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container-fluid">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-12">
             <div class="custom-accordion">
                 <div class="card">
-                    <div class="card-header bg-danger font-weight-bold text-center">
-                        <h4>Formulir Gugatan Cerai</h4> <!-- Ubah teks ini -->
+                    <div class="d-flex">
+                        <div class="card-header bg-danger font-weight-bold text-center">
+                            <h4>Pendaftaran Gugatan Cerai</h4>
+                        </div>
                     </div>
 
-                    <div class="card-body bg-info"> <!-- Ubah warna latar belakang ini -->
-                        <!-- Formulir Pendaftaran Gugatan Cerai -->
-                        <form method="POST" action="{{ route('gugatan_cerai.submit') }}">
+                    <div class="card-body bg-success">
+                        <!-- card-body content -->
+                        <form action="/submit" method="post">
                             @csrf
+                            <div class="row">
+                                <!-- Penggugat -->
+                                <div class="col">
+                                    <div class="form-group">
+                                        <label for="penggugat">Penggugat:</label>
+                                        <input type="text" name="penggugat" id="penggugat" class="form-control" required>
+                                    </div>
+                                </div>
 
-                            <!-- Informasi Penggugat -->
-                            <h4>Informasi Penggugat</h4>
-                            <div class="container">
+                                <!-- Tergugat -->
+                                <div class="col">
+                                    <div class="form-group">
+                                        <label for="tergugat">Tergugat:</label>
+                                        <input type="text" name="tergugat" id="tergugat" class="form-control" required>
+                                    </div>
+                                </div>
+                            </div>
 
-                                <!-- Nama Penggugat -->
-                                <div id="personal-data" class="collapse show">
-                                    <div class="row">
-                                        <div class="col-lg-6">
-                                            <div class="mb-3 mb-4">
-                                                <label class="form-label" for="nama_penggugat">Nama Penggugat:</label>
-                                                <input type="text" class="form-control" id="nama_penggugat" name="nama_penggugat" placeholder="Masukkan Nama Penggugat" value="{{ old('nama_penggugat') }}" required>
+                            <!-- Tempat Menikah -->
+                            <div class="form-group">
+                                <label for="tempat_menikah">Tempat Menikah:</label>
+                                <input type="text" name="tempat_menikah" id="tempat_menikah" class="form-control" required>
+                            </div>
+
+                            <!-- Tombol Submit -->
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary">Kirim Gugatan</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
