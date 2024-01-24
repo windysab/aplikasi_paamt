9741f3c2dafeb8eff753f80335e3c820ac7ad244
windys <windysabtami85@gmail.com>
2024-01-24 22:07:51 +0800

    opudate

--- app/Http/Controllers/GugatanCeraiController.php
+++ app/Http/Controllers/GugatanCeraiController.php
@@ -72,6 +72,71 @@ class GugatanCeraiController extends Controller
     }
 
 
+    // public function generateWordDocument($id)
+    // {
+    //     $gugatanCerai = GugatanCerai::find($id);
+
+    //     // Pastikan bahwa nama file template benar dan sesuai
+    //     $templatePath = resource_path('templates/Form Pendaftaran CG Bain (Simple).docx');
+
+    //     // Pastikan bahwa file template memang ada di path yang ditentukan
+    //     if (!file_exists($templatePath)) {
+    //         return redirect()->back()->with('error', 'File template tidak ditemukan!');
+    //     }
+
+    //     // Buat variabel baru untuk menyimpan hasil replace
+    //     $templateProcessor = new TemplateProcessor($templatePath);
+
+
+
+    //     // Replace variabel di template word dengan data yang diinputkan
+    //     $bulanIndonesia = [
+    //         'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni',
+    //         'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'
+    //     ];
+    //     $bulan = $bulanIndonesia[date('n') - 1]; // Mengambil nama bulan dari array berdasarkan bulan saat ini
+    //     $templateProcessor->setValue('tanggal', date('d') . ' ' . $bulan . ' ' . date('Y'));
+    //     $templateProcessor->setValue('nama_penggugat', $gugatanCerai->nama_penggugat);
+    //     $templateProcessor->setValue('binti_penggugat', $gugatanCerai->binti_penggugat);
+    //     $templateProcessor->setValue('umur_penggugat', $gugatanCerai->umur_penggugat);
+    //     $templateProcessor->setValue('agama_penggugat', $gugatanCerai->agama_penggugat);
+    //     $templateProcessor->setValue('pekerjaan_penggugat', $gugatanCerai->pekerjaan_penggugat);
+    //     $templateProcessor->setValue('pendidikan_penggugat', $gugatanCerai->pendidikan_penggugat);
+    //     $templateProcessor->setValue('alamat_penggugat', $gugatanCerai->alamat_penggugat);
+    //     $templateProcessor->setValue('nama_tergugat', $gugatanCerai->nama_tergugat);
+    //     $templateProcessor->setValue('umur_tergugat', $gugatanCerai->umur_tergugat);
+    //     $templateProcessor->setValue('bin_tergugat', $gugatanCerai->bin_tergugat);
+    //     $templateProcessor->setValue('agama_tergugat', $gugatanCerai->agama_tergugat);
+    //     $templateProcessor->setValue('pekerjaan_tergugat', $gugatanCerai->pekerjaan_tergugat);
+    //     $templateProcessor->setValue('pendidikan_tergugat', $gugatanCerai->pendidikan_tergugat);
+    //     $templateProcessor->setValue('alamat_tergugat', $gugatanCerai->alamat_tergugat);
+    //     $templateProcessor->setValue('alasan_cerai', $gugatanCerai->alasan_cerai);
+    //     $templateProcessor->setValue('alasan_cerai2', $gugatanCerai->alasan_cerai2);
+    //     $templateProcessor->setValue('alasan_cerai3', $gugatanCerai->alasan_cerai3);
+    //     $templateProcessor->setValue('separation_details', $gugatanCerai->separation_details);
+
+
+
+
+
+    //     // Buat nama file hasil replace yang baru
+    //     $filename = 'gugatan_cerai_' . $gugatanCerai->nama_penggugat . '.docx';
+    //     // $filename = 'gugatan_cerai_' . str_replace(['/', '\\'], '_', $gugatanCerai->nama_penggugat) . '.docx';
+
+    //     // Tentukan lokasi penyimpanan file hasil replace
+    //     $destinationFolder = 'C:\\Users\\user\\Documents\\uji coba\\';
+
+    //     // Membuat path lengkap ke file
+    //     $filepath = $destinationFolder . $filename;
+
+
+    //     // Simpan hasil TemplateProcessor sebagai file Word
+    //     $templateProcessor->saveAs($filepath);
+
+    //     // Kembalikan file untuk diunduh
+    //     return response()->download($filepath, $filename)->deleteFileAfterSend(true);
+    // }
+
     public function generateWordDocument($id)
     {
         $gugatanCerai = GugatanCerai::find($id);
@@ -86,8 +151,6 @@ class GugatanCeraiController extends Controller
 
         // Buat variabel baru untuk menyimpan hasil replace
         $templateProcessor = new TemplateProcessor($templatePath);
-
-
 
         // Replace variabel di template word dengan data yang diinputkan
         $bulanIndonesia = [
@@ -115,12 +178,9 @@ class GugatanCeraiController extends Controller
         $templateProcessor->setValue('alasan_cerai3', $gugatanCerai->alasan_cerai3);
         $templateProcessor->setValue('separation_details', $gugatanCerai->separation_details);
 
-        
-
-
-
-        // Buat nama file hasil replace yang baru
-        $filename = 'gugatan_cerai_' . $gugatanCerai->nama_penggugat . '.docx';
+        // Replace certain characters in the name with a hyphen
+        // $filename = 'gugatan_cerai_' . str_replace(['/', '\\', ':', '*', '?', '«', '<', '>', '|'], '-', $gugatanCerai->nama_penggugat) . '.docx';
+        $filename = 'gugatan_cerai_' . preg_replace("/[^A-Za-z0-9 ]/", '', $gugatanCerai->nama_penggugat) . '.docx';
 
         // Tentukan lokasi penyimpanan file hasil replace
         $destinationFolder = 'C:\\Users\\user\\Documents\\uji coba\\';
@@ -128,13 +188,16 @@ class GugatanCeraiController extends Controller
         // Membuat path lengkap ke file
         $filepath = $destinationFolder . $filename;
 
-
         // Simpan hasil TemplateProcessor sebagai file Word
         $templateProcessor->saveAs($filepath);
 
+        // Sanitize the fallback filename
+        $fallbackFilename = str_replace(['/', '\\', ':', '*', '?', '«', '<', '>', '|'], '-', $filename);
+
         // Kembalikan file untuk diunduh
-        return response()->download($filepath, $filename)->deleteFileAfterSend(true);
+        return response()->download($filepath, $fallbackFilename)->deleteFileAfterSend(true);
     }
+
     public function exportGugatanCerai()
     {
         return Excel::download(new GugatanCeraiExport, 'gugatan_cerai.xlsx');
